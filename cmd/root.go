460c927f29e193e9d9310c38ac8d199568e85365
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-06-22 18:52:13 +0330

    init project

---
+++ cmd/root.go
@@ -0,0 +1,31 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "url-shortener",
+	Short: "A brief description of your application",
+	Long: `Nothing but a little project to make a url shortener for fun.
+
+For using first run migrate command and then run serve and enjoy it. :)`,
+	//Run: func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
